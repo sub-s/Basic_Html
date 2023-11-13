c595c8776fc85083f68c8ac1dfa0a0c2e867ba0a
yongsub shin <spoon718@gmail.com>
2023-11-13 21:10:27 +0900

    tab

--- js/ui.js
+++ js/ui.js
@@ -67,42 +67,108 @@ var ui = {
 // 탭 메뉴
 
 
-function test() {
-    console.log(this)
-}
-test(this);
+// function test() {
+//     console.log(this)
+// }
+// test(this);
 
-const myObject = {
-    myMethod : function()  {
-        console.log(this); // 자기 자신을 가킨다. 
-    },
-    myMethod2 : () => {
-        console.log(this); // 전역 자신을 가킨다. 
+// const myObject = {
+//     myMethod : function()  {
+//         console.log(this); // 자기 자신을 가킨다. 
+//     },
+//     myMethod2 : () => {
+//         console.log(this); // 전역 자신을 가킨다. 
+//     }
+// };
+
+// myObject.myMethod();
+// myObject.myMethod2();
+
+// function regularFunction() {
+//     console.log(this); // 호출된 컨텍스트에 따라 결정됨
+// }
+// const arrowFunction = () => {
+//     console.log(this); // arrowFunction이 정의된 컨텍스트를 유지
+// };
+// regularFunction(); // 호출된 컨텍스트에 따라 달라짐
+// arrowFunction(); // arrowFunction이 정의된 컨텍스트를 유지
+
+
+
+
+// 탭에 대한 함수를 정의 하고 그 조건에 맞게 호출한다.
+function tabEvent(value){
+    // console.log(typeof value)
+    if(typeof value === 'string' || typeof value === 'object'){
+        tabString(value)
+       //console.log('문자열')
+    } else { 
+        tabNumber(value)
+        //console.log('숫자열')
     }
-};
-
-myObject.myMethod();
-myObject.myMethod2();
-
-function regularFunction() {
-    console.log(this); // 호출된 컨텍스트에 따라 결정됨
 }
-const arrowFunction = () => {
-    console.log(this); // arrowFunction이 정의된 컨텍스트를 유지
-};
-regularFunction(); // 호출된 컨텍스트에 따라 달라짐
-arrowFunction(); // arrowFunction이 정의된 컨텍스트를 유지
+
+
+function tabNumber(num){
+    const _this = event.currentTarget;
+    const tabButtons = document.querySelectorAll('.tab-menu > button');
+    const _index = (num !== undefined) ? num : Array.from(tabButtons).indexOf(_this);
+
+    tabButtons.forEach((item, index)=>{
+        const controls = document.querySelector(`#${item.getAttribute('aria-controls')}`)
+        if(index === _index){
+            item.classList.add('isActive');
+            controls.classList.add('isActive');
+        }else {
+            item.classList.remove('isActive');
+            controls.classList.remove('isActive');
+        }   
+    });
+}
+
+// 클릭한 문자열이 있으면.. 문자열을 받아오고..
+// 아니면 
+
+function tabString(str){
+    const _this = (typeof str === 'object') ? str : event.currentTarget;
+    const tabButtons = document.querySelectorAll('.tab-menu > button');
+    const _getStr = (typeof str === 'object') ? str.getAttribute('aria-controls') : str
+    const _id = (str !== undefined && typeof str === 'string') ? str : _this.getAttribute('aria-controls')
+    // console.log(_id, "id");
+
+    // console.log(str.getAttribute('aria-controls') , 'this?')
+
+    tabButtons.forEach((item, index)=>{
+        // console.log(controls , "controls")
+        const controls = document.getElementById(item.getAttribute('aria-controls'));
+        if(item.getAttribute('aria-controls') === _id){
+            item.classList.add('isActive');
+            controls.classList.add('isActive')
+        } else {
+            item.classList.remove('isActive');
+            controls.classList.remove('isActive')
+        }
+    })
+}
 
 
 
 
-function tabMenu(num) {
+
+function tabMenu(num) { // num 으로 크ㄹ릭한 파라미터 값이 들ㅓ온다.
     // console.log(this , "1");
     const _this = event.currentTarget;
     const tabButtons = document.querySelectorAll('.tab-menu button')
     //const _index = (num ? num : Array.from(tabButtons).indexOf(_this));
-    const _index = (num !== undefined) ? num : Array.from(tabButtons).indexOf(_this); // 클릭한 요소에 index 구하기
-
+    
+    const type = typeof(num)
+    console.log(type , "type")
+    const ex = type ? Number : String
+    
+    // 클릭한 요소에 index 구하기
+    // num 값이 들어오면 num을 받고 넘이 없을 경우 클릭한 인덱스 값이 넘어온다.
+    const _index = (num !== undefined) ? num : Array.from(tabButtons).indexOf(_this); 
+    
     tabButtons.forEach((item, index) => {
         //console.log(this, "2");
         const controls = document.getElementById(item.getAttribute('aria-controls'))
@@ -116,6 +182,7 @@ function tabMenu(num) {
         }
     });
 
+    
 
     //const tabContent = document.getElementById(id) // 클릭 했을 때 컨텐츠 아이디 값을 가져온다.
     //const tabContents = document.querySelectorAll('.tab-content div');
